11e22f5268c84ee9d0551338ceef0dd76280e238
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-01-26 13:46:21 +0530

    Add files via upload
    Check even or odd

---
+++ Check2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("It is even");
+    }
+    else{
+        printf("It is odd");
+    }
+    return 0;
+}
