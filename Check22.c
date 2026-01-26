17b674941f43111a91e2255e0a6c231be16f211a
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-01-26 13:40:58 +0530

    Add files via upload
    Calculate day of a week using Switch

---
+++ Check22.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        printf("January has 30 days");
+        break;
+
+         case 2:
+        printf("feb has 28 or 29 days");
+        break;
+
+         case 3:
+        printf("march has 30 days");
+        break;
+
+         case 4:
+        printf("April has 31 days");
+        break;
+
+         case 5:
+        printf("may has 30 days");
+        break;
+
+         case 6:
+        printf("june has 31 days");
+        break;
+
+         case 7:
+        printf("July has 30 days");
+        break;
+
+         case 8:
+        printf("Aug has 31 days");
+        break;
+
+         case 9:
+        printf("sep has 30 days");
+        break;
+
+         case 10:
+        printf("oct has 31 days");
+        break;
+
+         case 11:
+        printf("nov has 30 days");
+        break;
+
+         case 12:
+        printf("Dec has 31 days");
+        break;
+
+
+
+
+
+    }
+}
