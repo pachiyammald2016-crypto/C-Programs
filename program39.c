77c7a3c76bec1f109869d751e6161a5bb05269fc
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-01-27 22:58:43 +0530

    Add files via upload
    Taxi Fare Calculation

---
+++ program39.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        printf("800");
+        break;
+
+        case 2:
+        printf("1200");
+        break;
+
+        case 3:
+        printf("1800");
+        break;
+    }
+    return 0;
+}
