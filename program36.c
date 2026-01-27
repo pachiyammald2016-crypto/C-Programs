4bdcb8f9761a539bb28d3d633e06ff550ac401ce
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-01-27 22:56:59 +0530

    Add files via upload
    Electricity Bill Late Fine

---
+++ program36.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        printf("300");
+        break;
+
+        case 2:
+        printf("800");
+        break;
+
+        case 3:
+        printf("1500");
+        break;
+
+    }
+    return 0;
+}
