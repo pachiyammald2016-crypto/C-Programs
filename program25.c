0a176160c128555b6de91cbd3891751ca63dea95
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-01-27 22:50:15 +0530

    Add files via upload
    Exam Fee Calculation

---
+++ program25.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        printf("1200");
+        break;
+
+        case 2:
+        printf("800");
+        break;
+
+        case 3:
+        printf("500");
+        break;
+    }
+    return 0;
+}
