5f08ed90f53c0be3f1cef802e7bb0f20f66a0e80
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-01-27 22:54:36 +0530

    Add files via upload
     Water Bill Calculation

---
+++ program32.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        printf("1000");
+        break;
+
+        case 2:
+        printf("1500");
+        break;
+
+        case 3:
+        printf("2000");
+        break;
+    }
+    return 0;
+}
