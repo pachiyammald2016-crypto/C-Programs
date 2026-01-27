6086502338a88cecd1d6a2895b3712ee292cf653
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-01-27 22:57:33 +0530

    Add files via upload
     Online Shopping Delivery Mode

---
+++ program37.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int fee;
+    scanf("%d",&fee);
+    switch(fee){
+        case 1:
+        printf("0");
+        break;
+
+        case 2:
+        printf("500");
+        break;
+    }
+    return 0;
+}
