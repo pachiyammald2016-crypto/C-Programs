f5f36154cc50838ca8eb7bce9ffbbef0a8166462
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-01-27 22:34:10 +0530

    Add files via upload
    Grading system using switch

---
+++ program2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    switch(n/10){
+        case 3:
+        printf("Suplymentary");
+        break;
+        case 4:
+        printf("Grade D");
+        break;
+        case 5:
+        printf("Grade D");
+        break;
+        case 6:
+        printf("Grade c");
+        break;
+        case 7:
+        printf("Grade c");
+        break;
+        case 8:
+        printf("Grade B");
+        break;
+        case 9:
+        printf("Grade A");
+        break;
+        }
+    return 0;
+    
+}
