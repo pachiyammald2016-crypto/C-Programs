087eeb5953ed711329bc54b48095f1ec7a07f944
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-01-27 22:58:07 +0530

    Add files via upload
     College Hostel Fee

---
+++ program38.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        printf("500");
+        break;
+
+        case 2:
+        printf("1200");
+        break;
+    }
+    return 0;
+}
