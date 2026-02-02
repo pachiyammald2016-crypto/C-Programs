77a158fefbe3cc9ce9a9b0918ec4b23e3e4ba1b0
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-02 13:27:11 +0530

    Add files via upload
     Expense Balance Point

--- arr11.c
+++ arr11.c
@@ -3,17 +3,26 @@ int main(){
     int size;
     scanf("%d",&size);
     int arr[size];
-    int count1=0,count2=0;
     for(int i=0;i<size;i++){
         scanf("%d",&arr[i]);
-        if(arr[i]%2==0){
-            count1++;
-        }
-        else{
-            count2++;
-        }
     }
-    printf("even: %d\n",count1);
-    printf("odd: %d",count2);
+    int num=0;
+    int not=-1;
+    int mid=size/2;
+    int sum=0;
+    for(int j=0;j<size/2;j++){
+        sum+=arr[j];
+    }
+    int sum2=0;
+    for(int k=size-1;k>mid;k--){
+        sum2+=arr[k];
+    }
+    if(sum==sum2){
+        printf("%d",mid);
+    }
+    if(sum!=sum2){
+        printf("%d",not);
+    }
     return 0;
+
 }
