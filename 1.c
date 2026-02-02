39a8e800c6953f361acbda8d73f7be2e94688304
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-02 18:47:15 +0530

    Add files via upload
    sum and average of array elements

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(int j=0;j<size;j++){
+        sum+=arr[j];
+    }
+    printf("%d",sum);
+    return 0;
+}
