8c0aaf7062a20b54647a2f12017d86be4f06231e
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-02 13:25:14 +0530

    Add files via upload
    Find Missing Roll Number

--- arr5.c
+++ arr5.c
@@ -3,31 +3,17 @@ int main(){
     int size;
     scanf("%d",&size);
     int arr[size];
-    for(int i=0;i<size;i++){
+    int sum=0,num=0;
+    for(int i=0;i<size-1;i++){
         scanf("%d",&arr[i]);
+        num=size*(size+1)/2;
     }
-    int max=arr[0];
-    int count=0;
-    for(int j=0;j<size;j++){
-        if(arr[j]>max){
-            max=arr[j];
-            if(max==arr[j]){
-                count++;
-        }
+    int total=0;
+    int final=0;
+    for(int j=0;j<size-1;j++){
+        total+=arr[j];
+        final=num-total;
     }
-    }
-    int second=arr[0];
-    for(int k=0;k<size;k++){
-        if(arr[k]>second&&arr[k]<max){
-            second=arr[k];
-        }
-    }
-    if(count>1){
-        printf("highest repeated\n");
-    }
-    else{
-    printf("large: %d\n",max);
-    }
-    printf("second: %d",second);
+    printf("%d",final);
     return 0;
 }
