6e39d2067b867661441ea8f0de7f6d26001a9b54
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-02 18:48:38 +0530

    Add files via upload
    maximum element in an array

---
+++ 3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    int max=arr[0];
+    for(int j=0;j<size;j++){
+        if(arr[j]>max){
+            max=arr[j];
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
