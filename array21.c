8ad935e2ca89960304db93e465f3c4d1182cc0a0
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-03 21:11:15 +0530

    Add files via upload
     Department with Longest Even–Odd Alternating Pattern

---
+++ array21.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int index;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(arr[i][j]%2!=0){
+                index=i;
+            }
+        }
+    }
+    printf("%d",index);
+    return 0;
+}
