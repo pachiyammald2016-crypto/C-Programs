ee3fb1b04089250efd7352a0b28d106ec6be242b
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-05 19:08:59 +0530

    Add files via upload
    Find maximum element

---
+++ pk2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int max=0;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(arr[i][j]>max){
+                max=arr[i][j];
+            }
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
