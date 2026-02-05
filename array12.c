d4ddcbb196d2b3b0230f01433c02796ccd1bf196
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-05 11:54:31 +0530

    Add files via upload
     Most Frequent Value in the Entire Matrix

---
+++ array12.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(arr[i][j]==arr[i][j+1]){
+                
+            }
+        }
+    }
+    return 0;
+}
