b859f74ea104c49fa39e59c7821f8ae6371c42b0
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-06 16:35:54 +0530

    Add files via upload
    target counting

---
+++ 12d.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int target;
+    scanf("%d",&target);
+    int count=0;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(arr[i][j]==target){
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
