3589cbedccceafc26aad961b59102632c97f57b2
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-06 16:19:18 +0530

    Add files via upload
    minimum of an array

---
+++ 5d.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    int min=arr[0];
+    for(int i=0;i<size;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    printf("%d",min);
+    return 0;
+}
