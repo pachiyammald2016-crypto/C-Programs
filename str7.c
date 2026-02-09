b370f38c408649a472cdf50c63cced9868e9a5ca
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-09 20:42:37 +0530

    Add files via upload
    Consonant Counter in Speech Analysis

---
+++ str7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[20];
+    fgets(str,sizeof(str),stdin);
+    int count=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]=='\n'){
+            continue;
+        }
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
+            continue;
+        }
+         count++;
+    }
+    printf("%d",count);
+    return 0;
+}
