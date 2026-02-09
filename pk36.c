d67b4fe0fe7bf13f6bcb0b7be9e57d6f70839b6d
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-09 15:34:57 +0530

    Add files via upload
    printing non diagonal elements with zero

---
+++ pk36.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int num=0;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+           if(arr[i]==arr[j]){
+            num=arr[i][j];
+           }
+           else{
+            num=0;
+           }
+           printf("%d ",num);
+    }
+    printf("\n");
+}
+    return 0;
+}
