bd7d75a887ba53dfe31eb6e465cce6b960c069fd
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-09 15:24:29 +0530

    Add files via upload
    even number count of an array

---
+++ pk41.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int count=0;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(arr[i][j]%2==0){
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
