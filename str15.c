3ab509cc5f5e288ed236ac5f72dfea0142cd9458
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-09 20:46:50 +0530

    Add files via upload
    Word Counter in Text Editor

---
+++ str15.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[50];
+    fgets(str,sizeof(str),stdin);
+
+    int count=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]==' '){
+            count++;
+        }
+    }
+    printf("%d",count+1);
+    return 0;
+}
