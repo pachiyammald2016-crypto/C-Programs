276d218c35da0b8d514b65765e112e9c429c26b1
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-09 20:44:10 +0530

    Add files via upload
     Uppercase → Lowercase Converter (Database Normalizer)

---
+++ str10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[50];
+    fgets(str,sizeof(str),stdin);
+
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]>='A'&&str[i]<='Z'){
+            str[i]=str[i]+32;
+        }
+        printf("%c",str[i]);
+    }
+    return 0;
+}
