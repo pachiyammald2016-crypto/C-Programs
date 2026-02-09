800b12c46440d6fb3ceee0aa93070cbeeb76627a
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-09 20:44:40 +0530

    Add files via upload
     Digit Counter in Product IDs

---
+++ str11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[50];
+    fgets(str,sizeof(str),stdin);
+    int count=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]>='1'&&str[i]<='9'){
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
