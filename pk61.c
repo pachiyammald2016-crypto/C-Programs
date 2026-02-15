2e7434595e4b3e3937e6a65d2eb9ca99f78db92a
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-15 07:40:06 +0530

    Add files via upload
    reverse of an array using function

---
+++ pk61.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int sort(int arr[],int size){
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size-1-i;j++){
+            if(arr[j]<arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+int main(){
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    sort(arr,size);
+    for(int i=0;i<size;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
